2f495f85560f3def07961bf7466e1c4e8375bb91
mfaaiiq <faiqjb9090@gmail.com>
2024-03-06 14:16:10 +0700

    resolve dark mode issue

--- src/js/script.js
+++ src/js/script.js
@@ -1,82 +1,53 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
-
-
-// Navbar Fixed Position
-window.onscroll = function() {
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
     const toTop = document.querySelector('#to-top');
-
-    if(window.pageYOffset> fixedNav ) {
-        header.classList.add('navbar-fixed');
-        toTop.classList.remove('hidden');
-        toTop.classList.add('flex');
-    } else {
-        header.classList.remove('navbar-fixed')
-        toTop.classList.add('hidden');
-        toTop.classList.remove('flex');
-    }
-}
-
-
-
-
-// Hamburger
-
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu')
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
-})
-
-// click in the outside of Hamburger 
-
-window.addEventListener('click', function(e) {
-    if(e.target != hamburger && e.target != navMenu) {
-        hamburger.classList.remove('hamburger-active');
-        navMenu.classList.add('hidden');
-    }
-})
-
-
-// Toggle Dark-Mode
-
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
-
-darkToggle.addEventListener('click', function() {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-        localStorage.theme = 'dark';
-    } else {
-        html.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
-})
-
-// Switch Position of Toggle to 
-
-// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//     darkToggle.checked = true;
-//   } else {
-//     darkToggle.checked = false;
-//   }
-
-// Cek apakah tema gelap disetel di localStorage atau jika tema gelap dipilih berdasarkan preferensi pengguna
-// if (localStorage.theme === 'dark' || (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-//     darkToggle.checked = true; // Menandai kotak centang jika tema gelap aktif
-//   } else {
-//     darkToggle.checked = false; // Jika tidak, jangan centang kotak
-//   }
   
-
-if (localStorage.theme = 'dark' || (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-} else {
-    document.documentElement.classList.remove('dark')
-}
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add('navbar-fixed');
+      toTop.classList.remove('hidden');
+      toTop.classList.add('flex');
+    } else {
+      header.classList.remove('navbar-fixed');
+      toTop.classList.remove('flex');
+      toTop.classList.add('hidden');
+    }
+  };
+  
+  // Hamburger
+  const hamburger = document.querySelector('#hamburger');
+  const navMenu = document.querySelector('#nav-menu');
+  
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+  });
+  
+  // Klik di luar hamburger
+  window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+      hamburger.classList.remove('hamburger-active');
+      navMenu.classList.add('hidden');
+    }
+  });
+  
+  // Darkmode toggle
+  const darkToggle = document.querySelector('#dark-toggle');
+  const html = document.querySelector('html');
+  
+  darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+      html.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      html.classList.remove('dark');
+      localStorage.theme = 'light';
+    }
+  });
+  
+  // pindahkan posisi toggle sesuai mode
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
